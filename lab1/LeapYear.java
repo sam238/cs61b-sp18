38fb0fb0ca7d1a57676c1f1ebe37de474e7c9617
sam238 <gongweishen@126.com>
2025-09-10 23:17:40 +0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,8 +13,13 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
-
-    /** Must be provided an integer as a command line argument ARGS. */
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+    /* Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
